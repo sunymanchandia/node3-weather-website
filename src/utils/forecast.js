4cd63713ff4720ea6aa89699a1c9e6af0db0ea32
sunymanchandia <sunymanchandia@gmail.com>
2020-08-28 18:02:41 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request=require('request')
+const forecast=(latitude,longitude,callback)=>{
+    const url='http://api.weatherstack.com/current?access_key=cd7df8e704e2185a84a65a9eed4d6b31&query='+latitude+','+longitude+'&units=f'
+    request({url,json:true},(error,{body})=>{
+        if(error){
+        callback('Unable to connect to services',undefined)
+        }
+        else if(body.error){
+            callback('Unable to find the location',undefined)
+        }
+        else{
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+'°F outside. There is '+body.current.precip+'% chance of rain. It feels like '+body.current.feelslike+' degrees out.')
+        }
+    })
+}
+module.exports=forecast
